2277850fa5010716c267e29861cabab8a14674b8
jonandrop <jonandrop.ja@gmail.com>
2024-08-17 13:44:49 +0200

    Language fix

--- cherry-charm/src/interface/modal/Modal.tsx
+++ cherry-charm/src/interface/modal/Modal.tsx
@@ -19,53 +19,50 @@ const Modal = () => {
               <img className="modal-image" src="./images/cherry.png" />
               <img className="modal-image" src="./images/cherry.png" />
               <img className="modal-image" src="./images/cherry.png" />
-              <span> Paga 50 </span>
+              <span> Ganá 50 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/apple.png" />
               <img className="modal-image" src="./images/apple.png" />
               <img className="modal-image" src="./images/apple.png" />
-              <span> Paga 20 </span>
+              <span> Ganá 20 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/banana.png" />
               <img className="modal-image" src="./images/banana.png" />
               <img className="modal-image" src="./images/banana.png" />
-              <span> Paga 15 </span>
+              <span> Ganá 15 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/lemon.png" />
               <img className="modal-image" src="./images/lemon.png" />
               <img className="modal-image" src="./images/lemon.png" />
-              <span> Paga 3 </span>
+              <span> Ganá 3 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/cherry.png" />
               <img className="modal-image" src="./images/cherry.png" />
-              <span> Paga 40 </span>
+              <span> Ganá 40 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/apple.png" />
               <img className="modal-image" src="./images/apple.png" />
-              <span> Paga 10 </span>
+              <span> Ganá 10 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/banana.png" />
               <img className="modal-image" src="./images/banana.png" />
-              <span> Paga 5 </span>
+              <span> Ganá 5 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               Ten en cuenta que las máquinas tragamonedas solo consideran un par como coincidencia si están en orden de izquierda a derecha.
-            </div>
-            <div className="modal-text">
-              Extra: Haz clic y mantén presionado en cualquier lugar y mueve el cursor para explorar la escena 3D.
             </div>
             <div>
               <div>
