97d57202e23b88bf7541919e2c18acb24467100d
jonandrop <jonandrop.ja@gmail.com>
2024-08-17 13:44:49 +0200

    Language fix

--- cherry-charm/src/interface/modal/Modal.tsx
+++ cherry-charm/src/interface/modal/Modal.tsx
@@ -19,46 +19,46 @@ const Modal = () => {
               <img className="modal-image" src="./images/cherry.png" />
               <img className="modal-image" src="./images/cherry.png" />
               <img className="modal-image" src="./images/cherry.png" />
-              <span> Paga 50 </span>
+              <span> Ganá 50 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/apple.png" />
               <img className="modal-image" src="./images/apple.png" />
               <img className="modal-image" src="./images/apple.png" />
-              <span> Paga 20 </span>
+              <span> Ganá 20 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/banana.png" />
               <img className="modal-image" src="./images/banana.png" />
               <img className="modal-image" src="./images/banana.png" />
-              <span> Paga 15 </span>
+              <span> Ganá 15 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/lemon.png" />
               <img className="modal-image" src="./images/lemon.png" />
               <img className="modal-image" src="./images/lemon.png" />
-              <span> Paga 3 </span>
+              <span> Ganá 3 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/cherry.png" />
               <img className="modal-image" src="./images/cherry.png" />
-              <span> Paga 40 </span>
+              <span> Ganá 40 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/apple.png" />
               <img className="modal-image" src="./images/apple.png" />
-              <span> Paga 10 </span>
+              <span> Ganá 10 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
               <img className="modal-image" src="./images/banana.png" />
               <img className="modal-image" src="./images/banana.png" />
-              <span> Paga 5 </span>
+              <span> Ganá 5 </span>
               <img className="modal-image" src="./images/coin.png" />
             </div>
             <div className="modal-text">
