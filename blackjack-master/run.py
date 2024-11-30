14cb22174d65ee3f64dceab49722713e8f949d9d
Jonandrop <jonandrop.ja@gmail.com>
2024-11-30 20:33:48 +0100

    Untrack all files that are now in .gitignore

--- blackjack-master/run.py
+++
@@ -1,3 +0,0 @@
-from . import app
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5003)  # Define the port here
--- session_management/imperioApp/utils/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'  # Replace with a secure random value
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///app.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CHERRY_CHARM_URL = 'http://localhost:5173'
-    BLACK_JACK_URL = 'http://localhost:5174'
